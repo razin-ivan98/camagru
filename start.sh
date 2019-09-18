bb20a9e17d6ec21726ba30295953175848cc8db7
Chaurus Orange dartwing <chorange@ox-w1.21-school.ru>
2019-09-18 15:38:55 +0300

    g

--- start.sh
+++ start.sh
@@ -10,8 +10,8 @@ docker rm camagru_phpmyadmin
 docker rm camagru_mysql
 
 docker run --name camagru_php -d -v $(pwd)/camagru:/app -p 8080:80 webdevops/php-apache-dev
-docker run --name camagru_phpmyadmin -d --link camagru_mysql:db -p 8081:80 phpmyadmin/phpmyadmin
 docker run --name camagru_mysql -d -v camagru_db:/var/lib/mysql --restart on-failure:5 -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=base mysql:5 --default-authentication-plugin=mysql_native_password
+docker run --name camagru_phpmyadmin -d --link camagru_mysql:db -p 8081:80 phpmyadmin/phpmyadmin
 
 print "docker-machine:"
 docker-machine ls | grep -E "tcp" | awk -F' ' '{print $5}' | awk -Ftcp:// '{print $2}' | awk -F: '{print $1}'
