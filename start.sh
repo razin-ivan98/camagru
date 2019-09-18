d50ded514dcb5d99c8658fd91b602910ac096667
Chaurus Orange dartwing <chorange@vo-j4.21-school.ru>
2019-09-18 15:16:49 +0300

    g

--- start.sh
+++ start.sh
@@ -11,4 +11,7 @@ docker run --name camagru_php -d -v $(pwd)/camagru:/app -p 8080:80 webdevops/php
 docker run --name camagru_phpmyadmin -d --link camagru_mysql:db -p 8081:80 phpmyadmin/phpmyadmin
 docker run --name camagru_mysql -d -v camagru_db:/var/lib/mysql --restart on-failure:5 -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=base mysql:5 --default-authentication-plugin=mysql_native_password
 
+print "docker-machine:"
+docker-machine ls | grep -E "tcp" | awk -F' ' '{print $5}' | awk -Ftcp:// '{print $2}' | awk -F: '{print $1}'
+print "mysql:"
 docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' camagru_mysql
